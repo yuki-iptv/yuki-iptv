0923d3bb69d7704985eac0b9a3d489065bc1bdcb
Astroncia <8614716-astroncia@users.noreply.gitlab.com>
2021-04-13 22:53:17 +0300

    TV guide caching bugfix, part 2

--- astroncia_iptv.py
+++ astroncia_iptv.py
@@ -203,7 +203,7 @@ if __name__ == '__main__':
                     cm3uf1 = open(str(Path(LOCAL_DIR, 'playlist.json')), 'r')
                     cm3u1 = json.loads(cm3uf1.read())
                     cm3uf1.close()
-                    epg_url1 = cm3u['epgurl']
+                    epg_url1 = cm3u1['epgurl']
                     if not settings["epg"]:
                         settings["epg"] = epg_url1
                 if tvguide_c1 != settings["epg"]:
