17fe1909f7fa2ecb7bb238b5963652a28b825432
Astroncia <8614716-astroncia@users.noreply.gitlab.com>
2021-04-20 05:38:18 +0300

    Fix hardware acceleration

--- astroncia_iptv.py
+++ astroncia_iptv.py
@@ -2072,7 +2072,7 @@ if __name__ == '__main__':
             print_with_time('[{}] {}: {}'.format(loglevel, component, message))
 
         if settings['hwaccel']:
-            VIDEO_OUTPUT = 'gpu,vdpau,vaapi,opengl,direct3d,xv,x11'
+            VIDEO_OUTPUT = 'gpu,opengl,direct3d,xv,x11'
             HWACCEL = 'yes'
         else:
             VIDEO_OUTPUT = 'direct3d,xv,x11'
