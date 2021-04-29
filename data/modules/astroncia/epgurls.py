1ff547500f499df10e670120e6b4e9c6f0800c72
Astroncia <8614716-astroncia@users.noreply.gitlab.com>
2021-04-29 13:01:29 +0300

    Adding new EPG url

--- data/modules/astroncia/epgurls.py
+++ data/modules/astroncia/epgurls.py
@@ -20,5 +20,6 @@ EPG_URLS = [
     "http://www.teleguide.info/download/new3/xmltv.xml.gz",
     "https://iptvx.one/EPG_LITE",
     "http://programtv.ru/xmltv.xml.gz",
-    "http://epg.it999.ru/edem.xml.gz"
+    "http://epg.it999.ru/edem.xml.gz",
+    "https://iptv-org.github.io/epg/guides/tv.yandex.ru.guide.xml"
 ]
