04ca0763abf4be12e45af4dbeecd10ca0b4f812e
Astroncia <8614716-astroncia@users.noreply.gitlab.com>
2021-07-24 18:10:31 +0300

    Version 0.0.54

--- astroncia_iptv.py
+++ astroncia_iptv.py
@@ -78,7 +78,7 @@ if not os.name == 'nt':
     except: # pylint: disable=bare-except
         print("Failed to init MPRIS libraries!")
 
-APP_VERSION = '0.0.53'
+APP_VERSION = '0.0.54'
 
 if not sys.version_info >= (3, 4, 0):
     print_with_time("Incompatible Python version! Required >= 3.4")
