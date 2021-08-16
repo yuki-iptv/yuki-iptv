d448985f328ad55927633a3bd4494526a40375cb
Astroncia <8614716-astroncia@users.noreply.gitlab.com>
2021-08-16 21:58:29 +0300

    Update new_version.sh

--- new_version.sh
+++ new_version.sh
@@ -9,3 +9,4 @@ echo "Using version: ${ver}"
 sed -i "s/APP_VERSION = '.*/APP_VERSION = '${ver}'/g" ./usr/lib/astronciaiptv/astroncia_iptv.py
 echo '{"version": "'"${ver}"'"}' > version.txt
 debchange --distribution unstable -M
+git add . && git commit -m "${ver}"
