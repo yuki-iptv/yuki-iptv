0de73f88450d86ee0383231906a00720e721a47b
Astroncia <8614716-astroncia@users.noreply.gitlab.com>
2021-08-16 21:55:33 +0300

    Add new_version.sh

---
+++ new_version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
+ver="$1"
+if [ "$ver" = "" ]; then
+echo No version specified
+exit 1
+fi
+echo "Using version: ${ver}"
+sed -i "s/APP_VERSION = '.*/APP_VERSION = '${ver}'/g" ./usr/lib/astronciaiptv/astroncia_iptv.py
+echo '{"version": "'"${ver}"'"}' > version.txt
+debchange --distribution unstable -M
