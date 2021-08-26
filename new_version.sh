7d104faa9725ff73845839e0d0edcc0d8f9dd057
Astroncia <8614716-astroncia@users.noreply.gitlab.com>
2021-08-26 06:08:34 +0300

    Fix versioning

--- new_version.sh
+++ new_version.sh
@@ -6,7 +6,6 @@ echo No version specified
 exit 1
 fi
 echo "Using version: ${ver}"
-sed -i "s/APP_VERSION = '.*/APP_VERSION = '${ver}'/g" ./usr/lib/astronciaiptv/astroncia_iptv.py
 echo '{"version": "'"${ver}"'"}' > version.txt
 debchange --distribution unstable -M
 git add . && git commit -m "${ver}"
