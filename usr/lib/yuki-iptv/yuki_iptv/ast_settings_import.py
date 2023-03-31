052787ca5f20d19b1b86c7955dee4c70186f2109
yuki-chan-nya <129041041+yuki-chan-nya@users.noreply.github.com>
2023-03-31 01:18:22 +0000

    Fix custom channel sort in multiple playlists with same channel name

--- usr/lib/yuki-iptv/yuki_iptv/ast_settings_import.py
+++ usr/lib/yuki-iptv/yuki_iptv/ast_settings_import.py
@@ -118,3 +118,34 @@ def convert_old_filenames():
         ) as new_favourites_file:
             new_favourites_file.write(json.dumps(new_favourites))
         os.remove(Path(os.environ['HOME'], '.config', 'yuki-iptv', 'favourites.json'))
+
+    if os.path.isfile(Path(os.environ['HOME'], '.config', 'yuki-iptv', 'sort.json')) \
+    and not os.path.isfile(
+        Path(os.environ['HOME'], '.config', 'yuki-iptv', 'sortchannels.json')
+    ):
+        with open(
+            Path(os.environ['HOME'], '.config', 'yuki-iptv', 'sort.json'),
+            'r', encoding="utf8"
+        ) as old_sort_file:
+            old_sort = json.loads(old_sort_file.read())
+
+        # settings start
+        settings = {'m3u': ''}
+        if os.path.isfile(Path(os.environ['HOME'], '.config', 'yuki-iptv', 'settings.json')):
+            with open(
+                Path(os.environ['HOME'], '.config', 'yuki-iptv', 'settings.json'),
+                'r', encoding="utf8"
+            ) as old_settings_file:
+                settings = json.loads(old_settings_file.read())
+        # settings end
+
+        new_sort = {}
+        if settings['m3u']:
+            new_sort[settings['m3u']] = old_sort
+
+        with open(
+            Path(os.environ['HOME'], '.config', 'yuki-iptv', 'sortchannels.json'),
+            'w', encoding="utf8"
+        ) as new_sort_file:
+            new_sort_file.write(json.dumps(new_sort))
+        os.remove(Path(os.environ['HOME'], '.config', 'yuki-iptv', 'sort.json'))
