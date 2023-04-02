eca7c0ee613bf2d39a30decf42124a7c76add1ef
yuki-chan-nya <129041041+yuki-chan-nya@users.noreply.github.com>
2023-04-02 16:56:10 +0000

    Add XMLTV test

---
+++ tests/test_xmltv.py
@@ -0,0 +1,80 @@
+#
+# Copyright (c) 2021-2022 Astroncia <kestraly@gmail.com>
+# Copyright (c) 2023 yuki-chan-nya
+#
+# This file is part of yuki-iptv.
+#
+# yuki-iptv is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# yuki-iptv is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with yuki-iptv  If not, see <http://www.gnu.org/licenses/>.
+#
+# The Font Awesome pictograms are licensed under the CC BY 4.0 License
+# https://creativecommons.org/licenses/by/4.0/
+#
+import os
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(os.getcwd(), 'usr', 'lib', 'yuki-iptv')))
+
+from yuki_iptv.xmltv import parse_as_xmltv  # noqa: E402
+
+
+def test_xmltv():
+    with open(Path('tests', 'xmltv.xml'), 'r', encoding='utf8') as xmltv_file_fd:
+        xmltv_file = xmltv_file_fd.read()
+    xmltv = parse_as_xmltv(xmltv_file, {"epgoffset": 0}, 1, {}, 0, "")
+    assert xmltv == [
+        {
+            'Test channel 1': [
+                {
+                    'start': 1680390000.0,
+                    'stop': 1680393600.0,
+                    'title': 'Test program 1',
+                    'desc': '',
+                    'catchup-id': ''
+                },
+                {
+                    'start': 1680393600.0,
+                    'stop': 1680397200.0,
+                    'title': 'Test program 2',
+                    'desc': 'Test description',
+                    'catchup-id': ''
+                }
+            ]
+        },
+        {'testchan': ['Test channel 1']},
+        {'Test channel 1': 'http://127.0.0.1/testchannel1.png'}
+    ]
+    xmltv1 = parse_as_xmltv(xmltv_file, {"epgoffset": -124}, 1, {}, 0, "")
+    assert xmltv1 == [
+        {
+            'Test channel 1': [
+                {
+                    'start': 1680390000.0 + (3600 * -124),
+                    'stop': 1680393600.0 + (3600 * -124),
+                    'title': 'Test program 1',
+                    'desc': '',
+                    'catchup-id': ''
+                },
+                {
+                    'start': 1680393600.0 + (3600 * -124),
+                    'stop': 1680397200.0 + (3600 * -124),
+                    'title': 'Test program 2',
+                    'desc': 'Test description',
+                    'catchup-id': ''
+                }
+            ]
+        },
+        {'testchan': ['Test channel 1']},
+        {'Test channel 1': 'http://127.0.0.1/testchannel1.png'}
+    ]
