438f431df65472ef349648e593618ee526414e4c
yuki-chan-nya <yukichandev@proton.me>
2023-04-08 16:01:46 +0000

    EPG: add more checks in JTV parser

--- usr/lib/yuki-iptv/yuki_iptv/epg_jtv.py
+++ usr/lib/yuki-iptv/yuki_iptv/epg_jtv.py
@@ -36,32 +36,52 @@ def parse_jtv(ndx, pdt, settings):
     if pdt[0:26] not in jtv_headers:
         logger.debug("Invalid PDT file!")
         return []
+
     schedules = []
+
+    if len(ndx[0:2]) != 2:
+        logger.debug("Invalid NDX file!")
+        return []
+
     total_num = struct.unpack('<H', ndx[0:2])[0]
     ndx = ndx[2:]
+
     for i in range(0, total_num):
         try:
             entry = ndx[i * 12:12 + (i * 12)]
-            if entry[0:2] != b'\x00\x00':
+
+            if len(entry[0:2]) != 2 or entry[0:2] != b'\x00\x00':
                 logger.debug("JTV format violation detected!")
+                continue
+
             if len(entry[2:10]) != 8:
                 logger.debug("Broken JTV time detected")
                 continue
             filetime = struct.unpack('<Q', entry[2:10])[0]
             start_time = (datetime.datetime(
-                year=1601, month=1, day=1
+                year=1601, month=1, day=1  # FILETIME
             ) + datetime.timedelta(
                 microseconds=filetime / 10
             )).timestamp() + (3600 * settings["epgoffset"])
+
+            if len(entry[10:12]) != 2:
+                logger.debug("Broken JTV offset detected")
+                continue
             offset = struct.unpack('<H', entry[10:12])[0]
+
+            if len(pdt[offset:offset + 2]) != 2:
+                logger.debug("Broken JTV count detected")
+                continue
             count = struct.unpack('<H', pdt[offset:offset + 2])[0]
+
             program_name = pdt[offset + 2:offset + 2 + count]
             try:
                 program_name = program_name.decode('utf-8')
             except UnicodeDecodeError:
                 program_name = program_name.decode('cp1251')
+
             if isinstance(program_name, str):
-                if count < 1000:  # workaround, do not allow broken files
+                if count < 1000:  # Workaround, do not allow broken entries
                     schedules.append({
                         'start': start_time,
                         'stop': 0,
