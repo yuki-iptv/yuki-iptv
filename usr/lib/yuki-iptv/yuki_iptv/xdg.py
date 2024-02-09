7714baa91b146e4eec11608e3a399dda3c07847a
Ame-chan-angel <amechanangel@proton.me>
2024-02-09 15:12:25 +0000

    Update xdg.py

--- usr/lib/yuki-iptv/yuki_iptv/xdg.py
+++ usr/lib/yuki-iptv/yuki_iptv/xdg.py
@@ -1,5 +1,6 @@
 #
-# Copyright (c) 2024 Ame-chan-angel <amechanangel@proton.me>
+# Copyright (c) 2021, 2022 Astroncia
+# Copyright (c) 2023, 2024 Ame-chan-angel <amechanangel@proton.me>
 #
 # This file is part of yuki-iptv.
 #
